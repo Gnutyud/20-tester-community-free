7d085683a1fb1c998c2c44dea1ba80c8965c7fc1
gnutyud <nguyenduytungtd3@gmail.com>
2024-07-16 10:58:24 +0700

    update responsive

--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export const Container = ({ children }: { children: ReactNode }) => {
   return (
     <div className="w-full flex justify-center">
-      <div className="lg:max-w-6xl lg:w-full">{children}</div>
+      <div className="md:max-w-6xl w-full">{children}</div>
     </div>
   );
 };
